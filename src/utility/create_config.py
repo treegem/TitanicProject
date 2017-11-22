c031654050e80b76c160607b52f6fdde57e4f587
Martin Glatzle <mglatzle@mpa-garching.mpg.de>
2017-11-22 21:50:19 +0100

    Use absolute paths.

--- src/utility/create_config.py
+++ src/utility/create_config.py
@@ -2,14 +2,17 @@ import configparser
 
 import os
 
-project_path = os.path.join(os.path.dirname(__file__), '../..')
+project_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), '../..'))
 
 config = configparser.ConfigParser()
 
 config['paths'] = {}
 config['paths']['project'] = project_path
 config['paths']['resources'] = os.path.join(project_path, 'resources')
-config['paths']['images'] = os.path.join(config['paths']['resources'], 'images')
+config['paths']['images'] = os.path.join(
+    config['paths']['resources'], 'images')
 
 config_path = os.path.join(project_path, 'config.ini')
 
