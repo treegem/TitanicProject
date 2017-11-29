cbf36fd39892aa9dcf941655afaf6c92408f4761
gbrown <georg.braunbeck@googlemail.com>
2017-11-29 18:52:14 +0100

    printing feature importances

--- src/decision_trees/random_forest.py
+++ src/decision_trees/random_forest.py
@@ -2,7 +2,7 @@ from scipy.stats import uniform, randint
 from sklearn.ensemble import RandomForestClassifier
 
 from src.utility.data_preparation import split_data, load_clean_data, standard_scale
-from utility.parameter_search import randomized_search_cv
+from src.utility.parameter_search import randomized_search_cv
 
 
 def main():
@@ -11,14 +11,14 @@ def main():
     data = standard_scale(data)
     clf = RandomForestClassifier()
 
-    param_dist = {'n_estimators': randint(low=1, high=10000),
-                  'max_depth': list(range(1, 50)) + [None],
+    param_dist = {'n_estimators': randint(low=50, high=10000),
+                  'max_depth': list(range(1, 20)) + [None],
                   'min_samples_split': list(range(2, 10)),
                   'min_samples_leaf': list(range(1, 10)),
                   'max_features': list(range(1, 10)) + [None],
                   'min_impurity_decrease': uniform(loc=0, scale=1)}
-    clf = randomized_search_cv(clf, data, parameter_distribution=param_dist, targets=y, n_jobs=3, n_iter=2000)
-    # n_jobs = number of parallel threads, should not exceed number of cores
+    clf = randomized_search_cv(clf, data, parameter_distribution=param_dist,
+                               targets=y, n_jobs=3, n_iter=100, cv=4, verbose=True)
 
     data = load_clean_data()
     data_train, data_val, y_train, y_val = split_data(data)
@@ -26,6 +26,9 @@ def main():
     score_train = clf.score(data_train, y_train)
     score_val = clf.score(data_val, y_val)
 
+    print('\nFeature importances:')
+    for i, column in enumerate(data_train.columns):
+        print(column, clf.feature_importances_[i])
     print('score_train:', score_train, '; score_val:', score_val)
 
 
--- src/decision_trees/single_tree.py
+++ src/decision_trees/single_tree.py
@@ -2,8 +2,8 @@ from scipy.stats import uniform
 from sklearn.tree import DecisionTreeClassifier
 
 from src.utility.data_preparation import load_clean_data, split_data
-from utility.data_preparation import standard_scale
-from utility.parameter_search import randomized_search_cv
+from src.utility.data_preparation import standard_scale
+from src.utility.parameter_search import randomized_search_cv
 
 
 def main():
@@ -17,8 +17,8 @@ def main():
                   'min_samples_leaf': list(range(1, 10)),
                   'max_features': list(range(1, 10)) + [None],
                   'min_impurity_decrease': uniform(loc=0, scale=1)}
-    clf = randomized_search_cv(clf, data, parameter_distribution=param_dist, targets=y, n_jobs=4)
-    # n_jobs = number of cores used, adjust for dual or single core
+    clf = randomized_search_cv(clf, data, parameter_distribution=param_dist,
+                               targets=y, n_jobs=3, n_iter=1000, cv=5, verbose=True)
 
     data = load_clean_data()
     data_train, data_val, y_train, y_val = split_data(data)
@@ -26,7 +26,10 @@ def main():
     score_train = clf.score(data_train, y_train)
     score_val = clf.score(data_val, y_val)
 
-    print('score_train:', score_train, '; score_val:', score_val)
+    print('\nFeature importances:')
+    for i, column in enumerate(data_train.columns):
+        print(column, clf.feature_importances_[i])
+    print('\nscore_train:', score_train, '; score_val:', score_val)
 
 
 if __name__ == '__main__':
