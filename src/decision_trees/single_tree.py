18aa2fecd18abc2127583621de7b405a354eb75c
findesgh <findessp@yandex.ru>
2017-12-29 23:09:29 +0100

    No need to split data set after fitting, can use full dataset to obtain score.

--- src/decision_trees/single_tree.py
+++ src/decision_trees/single_tree.py
@@ -1,7 +1,7 @@
 from scipy.stats import uniform
 from sklearn.tree import DecisionTreeClassifier
 
-from src.utility.data_preparation import load_clean_data, load_clean_split_standard_data
+from src.utility.data_preparation import load_clean_data
 from src.utility.data_preparation import standard_scale
 from src.utility.parameter_search import randomized_search_cv
 from src.utility.storage_utility import save_model
@@ -22,16 +22,13 @@ def main():
                                targets=y, n_jobs=3, n_iter=1000, cv=5,
                                verbose=True)
 
-    data_train, data_val, y_train, y_val = load_clean_split_standard_data()
-    score_train = clf.score(data_train, y_train)
-    score_val = clf.score(data_val, y_val)
-
+    score = clf.score(data, y)
     save_model(clf, file_name='single_tree')
 
     print('\nFeature importances:')
-    for i, column in enumerate(data_train.columns):
+    for i, column in enumerate(data.columns):
         print(column, clf.feature_importances_[i])
-    print('\nscore_train:', score_train, '; score_val:', score_val)
+    print('\nscore:', score)
 
 
 if __name__ == '__main__':
